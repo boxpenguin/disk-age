5f05bdc88900b3283c79adc31e4086ea8a1a4ebe
Jordan Cameron Uehara <rakunko@users.noreply.github.com>
2016-11-08 12:31:15 -0500

    Update disk-age.sh

--- disk-age.sh
+++ disk-age.sh
@@ -9,12 +9,18 @@ for i in a b c d e; do	# Gotta make this smarter
         {
 	echo "########################################################"
         smartctl -a /dev/sd$i | egrep 'rpm|Device Model|Model Family|SATA'
+        smartctl -a $i | egrep 'rpm|Device Model|Model Family|SATA'
+	MONTHS=$(smartctl -A $i | grep "Power_On_Hours" | awk '{print "Power On Years: " $10%8760/8760*12}')
 	MONTHS=$(smartctl -A /dev/sd$i | grep "Power_On_Hours" | awk '{print "Power On Years: " $10%8760/8760*12}')
+	YEARS=$(smartctl -A $i | grep "Power_On_Hours" | awk '{print "Power On Years: " $10/8760}')
 	YEARS=$(smartctl -A /dev/sd$i | grep "Power_On_Hours" | awk '{print "Power On Years: " $10/8760}')
+
 	# Need to remove the .###
 	echo "Hard Drive age: " $YEARS " Years and " $MONTHS " Months."
 	smartctl -A /dev/sd$i | grep "Temp" | awk '{print "Temp : " $10 " C"}'
+	smartctl -A $i | grep "Temp" | awk '{print "Temp : " $10 " C"}'
         lsblk | grep "sd$i"
+        lsblk | grep "$i"
 	} 2>&1 | tee -a $TMP
 done
 cat $TMP | aha --title "Age" > /var/www/html/_admin/Disk_Age.html	#Push to website
